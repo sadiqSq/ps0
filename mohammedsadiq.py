24e24bfbc4985b9a79f7ed67f6c213a6c8988ed3
sadiqSq <54506446+sadiqSq@users.noreply.github.com>
2019-08-25 17:15:45 +0100

    Add files via upload

---
+++ mohammedsadiq.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 14:59:51 2019
+
+@author: ALEDARE
+"""
+#U18/FNS/CSC/1061
+#MOHAMMED SADIQ
+import math
+x = int(input("Enter x Number:"))
+y  = int(input("Enter y Number:"))
+answer = x**y
+print (answer)
+print ("x raised to the power of y is:",answer)
+print (math.log(x,2))
+print (math.log2(x))
+print (x^y)
